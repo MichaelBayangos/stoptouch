98be8e5c6ed02a0d52cf3e4df7ad0e2323cb5a1f
MichaelBayangos <michaelbayangos34@gmail.com>
2023-10-06 17:39:48 +0800

    put some loading animation on login

--- lib/src/parentFeatures/parent_main.dart
+++ lib/src/parentFeatures/parent_main.dart
@@ -26,144 +26,147 @@ class _ParentMainPageState extends State<ParentMainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 40,
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Image.asset('assets/stoptouch.png'),
-                ),
-                const SizedBox(height: 60),
-                const Text(
-                  'Child Device Setup',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 60),
-                Container(
-                    padding: const EdgeInsets.only(left: 30),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Image.asset('assets/stoptouch.png'),
+                  ),
+                  const SizedBox(height: 60),
+                  const Text(
+                    'Child Device Setup',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 60),
+                  Container(
+                      padding: const EdgeInsets.only(left: 30),
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'PHONE LIMIT',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.only(left: 40, right: 40),
                     alignment: Alignment.centerLeft,
                     child: const Text(
-                      'PHONE LIMIT',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )),
-                const SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.only(left: 40, right: 40),
-                  alignment: Alignment.centerLeft,
-                  child: const Text(
-                    'Set limit how long to use phone then screen will automatically lock once the set time is reached.',
-                    style: TextStyle(fontSize: 11),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 30, 30, 5),
-                  child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      labelText: 'Time',
-                      border: OutlineInputBorder(),
-                      hintText: 'Timer for child device lock funtion',
-                      hintStyle: TextStyle(fontSize: 16),
-                      errorStyle: TextStyle(color: Colors.red),
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      'Set limit how long to use phone then screen will automatically lock once the set time is reached.',
+                      style: TextStyle(fontSize: 11),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please Enter time limit';
-                      }
-                      return null;
-                    },
-                    onSaved: (value) => timerValue = value!,
                   ),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                    padding: const EdgeInsets.only(left: 30),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 30, 30, 5),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Time',
+                        border: OutlineInputBorder(),
+                        hintText: 'Timer for child device lock funtion',
+                        hintStyle: TextStyle(fontSize: 16),
+                        errorStyle: TextStyle(color: Colors.red),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter time limit';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => timerValue = value!,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                      padding: const EdgeInsets.only(left: 30),
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'RESTRICTION LIMIT',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      )),
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.only(left: 40, right: 40),
                     alignment: Alignment.centerLeft,
                     child: const Text(
-                      'RESTRICTION LIMIT',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )),
-                const SizedBox(height: 10),
-                Container(
-                  padding: const EdgeInsets.only(left: 40, right: 40),
-                  alignment: Alignment.centerLeft,
-                  child: const Text(
-                    'Set how long you want to lock your device screen, activation after phone limit is done.',
-                    style: TextStyle(fontSize: 11),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(30, 30, 30, 80),
-                  child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: const InputDecoration(
-                      labelText: 'Time Restriction',
-                      border: OutlineInputBorder(),
-                      hintText:
-                          'Restrtiction time before the device unlock properly',
-                      hintStyle: TextStyle(fontSize: 16),
-                      errorStyle: TextStyle(color: Colors.red),
-                      contentPadding:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      'Set how long you want to lock your device screen, activation after phone limit is done.',
+                      style: TextStyle(fontSize: 11),
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please Enter Restriction time';
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 30, 30, 80),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      decoration: const InputDecoration(
+                        labelText: 'Time Restriction',
+                        border: OutlineInputBorder(),
+                        hintText:
+                            'Restrtiction time before the device unlock properly',
+                        hintStyle: TextStyle(fontSize: 16),
+                        errorStyle: TextStyle(color: Colors.red),
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please Enter Restriction time';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) => restrictionValue = value!,
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 0,
+                      padding: const EdgeInsets.fromLTRB(80, 15, 80, 15),
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        dbRef.set(timerValue);
+                        dbref1.set(restrictionValue);
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Success'),
+                              content: const Text(
+                                  'Your settings have been saved, Wait for the child device to Log In to apply Stop Touch configurations.'),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text('Ok'))
+                              ],
+                            );
+                          },
+                        );
                       }
-                      return null;
                     },
-                    onSaved: (value) => restrictionValue = value!,
-                  ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                    child: const Text(
+                      'Set',
+                      style: TextStyle(color: Colors.black),
                     ),
-                    elevation: 0,
-                    padding: const EdgeInsets.fromLTRB(80, 15, 80, 15),
-                    backgroundColor: Colors.blueAccent,
                   ),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _formKey.currentState!.save();
-                      dbRef.set(timerValue);
-                      dbref1.set(restrictionValue);
-                      showDialog(
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: const Text('Success'),
-                            content: const Text(
-                                'Your settings have been saved, Wait for the child device to Log In to apply Stop Touch configurations.'),
-                            actions: [
-                              TextButton(
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const Text('Ok'))
-                            ],
-                          );
-                        },
-                      );
-                    }
-                  },
-                  child: const Text(
-                    'Set',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ),
-                const SizedBox(height: 90),
-              ],
+                  const SizedBox(height: 90),
+                ],
+              ),
             ),
           ),
         ),
